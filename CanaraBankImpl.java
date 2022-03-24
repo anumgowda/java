14ff239c817fbad8cadb5adecb183c6c49c72f5a
anumgowda <anu.xworkz@gmail.com>
2022-03-24 11:08:32 +0530

    abstraction example

---
+++ CanaraBankImpl.java
@@ -0,0 +1,14 @@
+package com.xwork.bankapp;
+
+public class CanaraBankImpl implements Atm{
+
+	@Override
+	public void swipe() {
+		// TODO Auto-generated method stub
+		System.out.println("canara bank card is swiped");
+		
+	}
+		
+	}
+
+
